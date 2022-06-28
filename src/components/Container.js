4fb6fec24333378dd0bcbed65fb9cc2dd4d2715a
Awais Abbas <awaisabbas47@gmail.com>
2022-06-28 19:37:54 +0500

    Update container component to add classname props

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,6 @@
-export default function Container({ children }) {
+export default function Container({ children, className }) {
     return (
-        <div className="max-w-screen-md mx-auto lg:px-2 px-4">
+        <div className={`max-w-screen-md mx-auto lg:px-2 px-4 ${className}`}>
             {children}
         </div>
     )
