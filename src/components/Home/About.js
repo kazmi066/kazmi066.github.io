44ccfb6ede8ee2e61cbc3ff9dbeb99d99b913066
Awais Abbas <awaisabbas47@gmail.com>
2022-07-07 17:46:58 +0500

    Add about section content

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,7 +1,6 @@
 export default function About() {
     return (
         <div className="pt-16">
-            <h1 className="text-3xl font-bold pb-4">Hello!</h1>
             <h4 className="lg:w-11/12">
                 I'm a <b>software engineer</b> experienced in building web apps for all kinds of devices. I'm based in <b> Islamabad, Pakistan</b> where I work as a Full-stack Engineer.
                 <br />
