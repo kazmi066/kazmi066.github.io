fc877205073858ef1c86849b1706068448008800
Awais Abbas <awaisabbas47@gmail.com>
2022-07-07 18:49:41 +0500

    Update front animation

--- src/components/FrontLoader.js
+++ src/components/FrontLoader.js
@@ -2,13 +2,33 @@ import { motion } from "framer-motion";
 import { useEffect } from "react";
 import { useApp } from "../context";
 
+function SplitText({ children, ...rest }) {
+    let words = children.split(' ')
+    return words.map((word, i) => {
+        return (
+        <div
+            key={children + i}
+            style={{ display: 'inline-block', overflow: 'hidden' }}
+        >
+            <motion.div
+                {...rest}
+                style={{ display: 'inline-block', willChange: 'transform' }}
+                custom={i}
+            >
+                {word + (i !== words.length - 1 ? '\u00A0' : '')}
+            </motion.div>
+        </div>
+        )
+    })
+}
+
 export default function FrontLoader({ children }) {
     const { loading, setLoading } = useApp();
 
     useEffect(() => {
         setTimeout(() => {
             setLoading(false);
-        }, 1500)
+        }, 1000)
     }, [setLoading])
 
     return (
@@ -16,10 +36,24 @@ export default function FrontLoader({ children }) {
             {
                 loading ?
                     <div className="flex items-center justify-center h-screen w-full">
-                        <motion.h1
-                            animate={{ opacity: [.5, .7, .2, 1] }}
-                            transition={{ duration: 3 }}
-                            className="font-bold text-6xl">A.</motion.h1>
+                        <motion.div
+                            initial={{ opacity: 1 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            className="font-bold text-4xl">
+                            <SplitText
+                                initial={{ y: '100%' }}
+                                animate="visible"
+                                variants={{
+                                visible: i => ({
+                                            y: 0,
+                                            transition: {
+                                            delay: i * 0.1
+                                        }
+                                    })
+                                }}>Awais Abbas
+                            </SplitText>    
+                        </motion.div>
                     </div> :
                     children
             }
