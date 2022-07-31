d4e98c612b3e2534bc7317032487e4b2f45b31f0
Awais Abbas <awaisabbas47@gmail.com>
2022-07-31 14:03:24 +0500

    Update contact component to add id attribute

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -2,7 +2,7 @@ import Title from "../../Elements/Title";
 
 export default function Contact() {
     return (
-        <div className="mt-12">
+        <div className="mt-12" id="contact">
             <Title title="contact" />
             <h4 className="pt-8">If you want to reach out please drop me an <a href="mailto:awaisabbas47@gmail.com" className="text-purple-500 hover:underline">Email</a>. You can also check out my social media presence following the links below.</h4>
         </div>
