ae78201264238f91b853bf4471ddd522f254aef7
Awais Abbas <awaisabbas47@gmail.com>
2022-10-24 13:19:10 +0500

    Update bio

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -2,9 +2,13 @@ export default function About() {
     return (
         <div className="mt-8">
             <h4>
-                I'm a <b>software engineer</b> experienced in building web apps for all kinds of devices. I'm based in <b> Islamabad, Pakistan</b> where I work as a Full-stack Engineer.
+                I'm a software engineer specializing in building exceptional websites.
+                My interest in web development starting back in 2020 during my university - turns out making CRUD operations
+                for a semester project made me curios about the web industry.
                 <br />
-                My focus right now is <b>react native</b>, <b>graphql</b> and <b>typeScript</b>. I also have extensive experience with <b>NodeJS</b> and <b>NextJS</b>.
+                My focus right now is <b>React</b>, <b>Next</b> and <b>Typescript</b>.
+                Might also try <b>React Native</b> someday.
+                I also have experience with backend stuff including <b>NodeJs / Express</b> and <b>Graphql</b>.
             </h4>
         </div>
     )
