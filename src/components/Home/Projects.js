754ea3de8e2f2180fc871c51b729cce3f1782a96
Awais Abbas <awaisabbas47@gmail.com>
2022-12-14 22:08:27 +0500

    Add tabs to projects screen

--- src/components/Home/Projects.js
+++ src/components/Home/Projects.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import projects from '../../data/projects.json';
 import Title from '../../Elements/Title'
 import { ProjectCard } from '../ProjectCard';
 import 'react-tabs/style/react-tabs.css';
@@ -7,78 +8,36 @@ import 'react-tabs/style/react-tabs.css';
 export default function Projects() {
 	const ref = useRef();
 
-	const projects = [
-		{
-			id: 2,
-			basicTitle: 'Ledger',
-			hoveredTitle: 'Ledger',
-			description: 'A fiverr client project dealing with blockchain, built with Next and Tailwind.',
-			link: 'https://ledger-design.netlify.app',
-			backgroundImg: 'https://media.giphy.com/media/gBxL0G0DqZd84/giphy.gif',
-			category: 'next'
-		},
-		{
-			id: 3,
-			basicTitle: 'Febeth',
-			hoveredTitle: 'Febeth Tokens',
-			description: 'A frontend project for client regarding blockchain tokens, built with Next and Tailwind.',
-			link: 'https://febeth-redesign.netlify.app',
-			backgroundImg: 'https://media.giphy.com/media/8LpEhnKQ4olb2/giphy.gif',
-			category: 'next'
-		},
-		{
-			id: 4,
-			basicTitle: 'Shroombits',
-			hoveredTitle: 'Shroombits',
-			description: 'A frontend project for a fiverr client regarding NFTs, built with React and Tailwind.',
-			link: 'https://shroombits-102471.netlify.app',
-			backgroundImg: 'https://media.giphy.com/media/VeevaQ0W85jzy/giphy.gif',
-			category: 'react'
-		},
-		{
-			id: 1,
-			basicTitle: 'Unsplash Clone',
-			hoveredTitle: 'Unsplash-clone',
-			description: 'Responsive images gallery website built with React and Tailwind.',
-			link: 'https://unsplash-clone-d3.netlify.app/',
-			backgroundImg: 'https://media.giphy.com/media/11y9UBjLh2Rs40/giphy.gif',
-			category: 'react'
-		}
-	]
+	const renderProjects = (category) => {
+		return (
+			projects.map((project) => (
+				project.category === category
+				&& <React.Fragment key={project.id}>
+					<ProjectCard project={project} />
+				</React.Fragment>
+			))
+		)
+	}
 
-  return (
-	<section className="pt-12">
-		<Title title="Projects" />
-		<Tabs className="mt-10">
-			<TabList className="border-b-[1px] border-gray-600">
-				<Tab>React.js</Tab>
-				<Tab>Next.js</Tab>
-			</TabList>
-			<TabPanel className="mt-8">
-				<div className="flex overflow-scroll gap-4 items-center" ref={ref}>
-					{
-						projects.map((project) => (
-							project.category === 'react'
-							&& <React.Fragment key={project.id}>
-								<ProjectCard project={project} />
-							</React.Fragment>
-						))
-					}
-				</div>
-			</TabPanel>
-			<TabPanel className="mt-8">
-			<div className="flex overflow-scroll gap-4 items-center" ref={ref}>
-					{
-						projects.map((project) => (
-							project.category === 'next'
-							&& <React.Fragment key={project.id}>
-								<ProjectCard project={project} />
-							</React.Fragment>
-						))
-					}
-				</div>
-			</TabPanel>
-		</Tabs>
-	</section>
-  )
+	return (
+		<section className="pt-12">
+			<Title title="Projects" />
+			<Tabs className="mt-10 sticky top-0">
+				<TabList className="border-b-[1px] border-gray-600">
+					<Tab>React.js</Tab>
+					<Tab>Next.js</Tab>
+				</TabList>
+				<TabPanel className="mt-8">
+					<div className="overflow-hidden grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4" ref={ref}>
+						{renderProjects('react')}
+					</div>
+				</TabPanel>
+				<TabPanel className="mt-8">
+					<div className="overflow-hidden grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4" ref={ref}>
+						{renderProjects('next')}
+					</div>
+				</TabPanel>
+			</Tabs>
+		</section>
+	)
 }
