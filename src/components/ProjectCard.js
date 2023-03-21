2bf78328de21bbc09eb6851739cee99a8333348a
Awais Abbas <awaisabbas47@gmail.com>
2023-03-21 21:27:00 +0500

    Add zeej to projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,8 +1,8 @@
 export const ProjectCard = ({ project }) => {
   return (
     <div key={project.id} className="text-white">
-        <div style={{ backgroundColor: "black", backgroundImage: `url(${project.backgroundImg})` }} className={`relative inline-block overflow-hidden mb-4 sm:h-72 h-44 w-full px-4 rounded-xl bg-no-repeat bg-cover`}>
-            <h4 className="font-bold pt-4 leading-tight pb-2">
+        <div style={{ backgroundColor: "black", backgroundImage: `url(${project.backgroundImg})` }} className={`relative inline-block overflow-hidden mb-4 py-4 sm:h-72 h-auto w-full px-4 rounded-xl bg-no-repeat bg-cover`}>
+            <h4 className="font-bold leading-tight pb-2">
                 {project.basicTitle}
             </h4>
             <p className="mb-4 text-sm">
