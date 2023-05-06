4296c4e9d89b900f4a4298a31d7d999d3f70bf80
Awais Abbas <awaisabbas47@gmail.com>
2023-05-06 10:15:42 +0500

    Add starter template link to about

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -20,6 +20,12 @@ export default function About() {
                 I'm diving into {highlightContainer("React Native")} too, because who doesn't want to conquer the world of mobile apps? But that's not all—I've also got some backend skills under my belt, including {highlightContainer("Node.js")}, {highlightContainer("Express")}, and {highlightContainer("GraphQL")}.
                 I'm a well-rounded developer who knows how to handle the whole shebang.
             </h4>
+            <div className="mt-8">
+                Have you checked out my React starter template?&nbsp;
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/kazmi066/react-tailwind-rtk-starter" className="text-orange-500">
+                    https://github.com/kazmi066/react-tailwind-rtk-starter
+                </a>
+            </div>
         </div>
     )
 }
