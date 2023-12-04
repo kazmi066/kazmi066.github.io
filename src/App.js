9c8cdb378f02828a58d75df99e6c0442688f5945
Awais Abbas <awaisabbas47@gmail.com>
2023-12-04 19:25:20 +0500

    Fix font loading issue during spinner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useLayoutEffect, useState } from "react";
+import { Spinner } from './components/spinner';
 import MyRoutes from "./MyRoutes";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+    useLayoutEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
+
   return (
-    <div className="App">
+    <>
+      {loading && (
+        <div className="absolute inset-0 z-40 bg-[#111010] flex h-screen w-full items-center justify-center">
+          <Spinner />
+        </div>
+      )}
       <MyRoutes />
-    </div>
+    </>
   );
 }
 
--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,26 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/Header';
-import { Spinner } from './components/spinner';
-import { useLayoutEffect, useState } from 'react';
 
 export default function MyRoutes() {
-    const [loading, setLoading] = useState(true);
-
-    useLayoutEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 2000);
-    }, []);
-    
-    if (loading) {
-        return (
-            <div className="flex h-screen w-full items-center justify-center">
-                <Spinner />
-            </div>
-        )
-    }
-
     return (
         <BrowserRouter>
             <Header />
