ff953143539075aae945c94cd5ea3336d0f3129b
Awais Abbas <awaisabbas47@gmail.com>
2025-10-05 16:09:37 +0500

    feat: add multi-window frog animation with cross-window communication

--- data/blogs.js
+++ data/blogs.js
@@ -1,16 +1,21 @@
 export const blogs = [
   {
-    id: 1,
+    id: 8,
+    title: "My component has too many props and I'm not ok",
+    url: "https://kazmi066.hashnode.dev/my-component-has-too-many-props-and-im-not-ok",
+  },
+  {
+    id: 7,
     title: "Lub-e-Lubab of State Management in React",
     url: "https://kazmi066.hashnode.dev/lub-e-lubab-of-state-management-in-react",
   },
   {
-    id: 2,
+    id: 6,
     title: "Implementing auth flow in React-native with Zustand",
     url: "https://kazmi066.hashnode.dev/implementing-auth-flow-in-react-native-with-zustand",
   },
   {
-    id: 3,
+    id: 5,
     title:
       "A Clear Understanding of the <h1> Element: Perspectives from SEO and Developers",
     url: "https://kazmi066.hashnode.dev/a-clear-understanding-of-the-h1-element-perspectives-from-seo-and-developers",
@@ -21,18 +26,18 @@ export const blogs = [
     url: "https://dev.to/kazmi066/protecting-your-javascript-code-the-principle-of-least-exposure-44c2",
   },
   {
-    id: 5,
+    id: 3,
     title: "Choosing the Right CSS Naming Convention for Your Project",
     url: "https://dev.to/kazmi066/choosing-the-right-css-naming-convention-for-your-project-3kj6",
   },
   {
-    id: 6,
+    id: 2,
     title: "Creating Dynamic form in React",
     url: "https://dev.to/kazmi066/creating-dynamic-form-in-react-1n6g",
   },
   {
-    id: 7,
+    id: 1,
     title: "Converting JSX to downloadable pdf in react",
     url: "https://dev.to/kazmi066/converting-jsx-to-downloadable-pdf-in-react-20bh",
-  }
+  },
 ];
